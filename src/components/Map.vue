abc09716d1dc57e2a567ae7a8ea19a46cd2b40a6
Noy Gafni <noy.gafni17@gmail.com>
2020-05-12 14:55:18 +0300

    add measure tooltip for circle and line (doesn't work with square)

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,37 +1,55 @@
 <template>
-  <vl-map style="height: 90vh">
-    <vl-view :zoom.sync="zoom" :center.sync="center"></vl-view>
+    <vl-map style="height: 90vh" @pointermove="showMeasureTooltip">
+      <vl-view :zoom.sync="zoom" :center.sync="center"></vl-view>
 
-    <vl-layer-vector :z-index="1">
-      <vl-source-vector :features.sync="features" ident="the-source"></vl-source-vector>
+      <vl-layer-vector :z-index="1">
+        <vl-source-vector :features.sync="features" ident="the-source"></vl-source-vector>
 
-      <vl-style-box>
-        <vl-style-stroke color="green"></vl-style-stroke>
-        <vl-style-fill color="rgba(255,255,255,0.5)"></vl-style-fill>
-      </vl-style-box>
-    </vl-layer-vector>
+        <vl-style-box>
+          <vl-style-stroke color="green"></vl-style-stroke>
+          <vl-style-fill color="rgba(255,255,255,0.5)"></vl-style-fill>
+        </vl-style-box>
+      </vl-layer-vector>
 
-    <vl-interaction-draw type="Circle" :geometry-function="squareStyleFunction()" source="the-source">
-      <vl-style-box>
-        <vl-style-stroke color="blue"></vl-style-stroke>
-        <vl-style-fill color="rgba(255,255,255,0.5)"></vl-style-fill>
-      </vl-style-box>
-    </vl-interaction-draw>
-    <vl-interaction-draw type="LineString" source="the-source" max-points="1">
-      <vl-style-box>
-        <vl-style-stroke color="blue"></vl-style-stroke>
-        <vl-style-fill color="rgba(255,255,255,0.5)"></vl-style-fill>
-      </vl-style-box>
-    </vl-interaction-draw>
+      <vl-interaction-draw 
+        type="Circle"
+        source="the-source"
+      >
+        <vl-style-box>
+          <vl-style-stroke color="blue"></vl-style-stroke>
+          <vl-style-fill color="rgba(255,255,255,0.5)"></vl-style-fill>
+        </vl-style-box>
+      </vl-interaction-draw>
+      <vl-interaction-draw
+          type="LineString"
+          source="the-source"
+          :max-points="1"
+          @drawstart="measureDrawnLine"
+          @drawend="finishDrawing"
+      >
+        <vl-style-box>
+          <vl-style-stroke color="blue"></vl-style-stroke>
+          <vl-style-fill color="rgba(255,255,255,0.5)"></vl-style-fill>
+        </vl-style-box>
+      </vl-interaction-draw>
 
-    <vl-layer-tile :z-index="0">
-      <vl-source-osm></vl-source-osm>
-    </vl-layer-tile>
-  </vl-map>
+      <vl-layer-tile :z-index="0">
+        <vl-source-osm></vl-source-osm>
+      </vl-layer-tile>
+      <MouseMeasureTooltip
+      v-if="lineGeometry !== null && currentLength !== null"
+      :text="currentLength"
+      :positionX="mousePosistionX"
+      :positionY="mousePosistionY"
+    />
+    </vl-map>
 </template>
 
 <script>
+/*eslint-disable vue/no-unused-components*/
   import { createRegularPolygon } from 'ol/interaction/Draw';
+  import { getLength } from 'ol/sphere';
+  import MouseMeasureTooltip from './MouseMeasureTooltip';
 
   export default {
     data () {
@@ -39,11 +57,37 @@
         zoom: 2,
         center: [0, 0],
         features: [],
+        currentLength: null,
+        lineGeometry: null,
+        mousePosistionX: null,
+        mousePosistionY: null,
       }
     },
+    components: {
+      MouseMeasureTooltip
+    },
     methods: {
-      squareStyleFunction() {
+      squareGeometryFunction() {
         return createRegularPolygon(4);
+      },
+      formatLength(line) {
+        const length = getLength(line);
+        return length > 100 ? `${Math.round(length / 1000 * 100) / 100} km`
+          : `${Math.round(length * 100) / 100} m`;
+      },
+      measureDrawnLine(event) {
+        this.lineGeometry = event.feature.getGeometry();
+      },
+      showMeasureTooltip(event) {
+        if (this.lineGeometry !== null) {
+          this.currentLength = this.formatLength(this.lineGeometry);
+          this.mousePosistionX = event.pointerEvent.clientX + 15;
+          this.mousePosistionY = event.pointerEvent.clientY;
+        }
+      },
+      finishDrawing() {
+        this.lineGeometry = null;
+        this.currentLength = null;
       }
     }
   }
---
+++ src/components/MouseMeasureTooltip.vue
@@ -0,0 +1,34 @@
+<template>
+    <div 
+    class="tooltip"
+    :style="`left: ${positionX}px; top: ${positionY}px`"
+    >
+        {{ text }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: String,
+        positionX: Number,
+        positionY: Number
+    }
+}
+</script>
+
+<style scoped>
+
+.tooltip {
+    position: absolute;
+    background: rgba(0, 0, 0, 0.5);
+    border-radius: 4px;
+    color: white;
+    padding: 4px 8px;
+    opacity: 0.7;
+    white-space: nowrap;
+    font-size: 12px;
+    z-index: 30;
+}
+
+</style>
